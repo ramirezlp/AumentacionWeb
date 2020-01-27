274d0c89bf1d28c692cfcd132dae47deb40b6400
ramirezlp <agusedlp1@gmail.com>
2020-01-27 13:09:10 -0300

    google y bing search funcionando

--- src/background_scripts/main.js
+++ src/background_scripts/main.js
@@ -1,28 +1,35 @@
-class BackgroundResult{
-    consultarSitio(){
-        console.log('holaaaaaa')
-		this.getCurrentTab().then((tabs) => {
-			browser.tabs.sendMessage(tabs[0].id, {
-				call: "consultarSitio"
-			});
-		});
+class BackgroundResult {
+    consultarSitio() {
+        this.getCurrentTab().then((tabs) => {
+            browser.tabs.sendMessage(tabs[0].id, {
+                call: "consultarSitio"
+            });
+        });
+    }
+    retrieveGoogleSearch(){
+        this.getCurrentTab().then((tabs) => {
+            browser.tabs.sendMessage(tabs[0].id, {
+                call: "retrieveGoogleSearch"
+            });
+        });
     }
     getCurrentTab(callback) {
-		return browser.tabs.query({
-			active: true,
-			currentWindow: true
-		});
-	}
+        return browser.tabs.query({
+            active: true,
+            currentWindow: true
+        });
+    }
 }
 
 var extension = new BackgroundResult();
 browser.browserAction.onClicked.addListener(() => {
-	  extension.consultarSitio();
+    extension.consultarSitio();
+    extension.retrieveGoogleSearch();
 });
 
 browser.runtime.onMessage.addListener((request, sender) => {
-	console.log("[background-side] calling the message: " + request.call);
-	if(extension[request.call]){
-		return extension[request.call](request.args);
-	}
+    console.log("[background-side] calling the message: " + request.call);
+    if (extension[request.call]) {
+        return extension[request.call](request.args);
+    }
 });
--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -1,7 +1,84 @@
 class Result {
-    consultarSitio(){
-        console.log('Te encuentras en:',document.location['origin'])
-        document.querySelector("body").style["background-color"] = "yellow";
+    consultarSitio() {
+        console.log('Te encuentras en:', document.location['origin'])
+        //document.querySelector("body").style["background-color"] = "yellow";
+        var div = this.createContainer("300px", window.innerHeight + "px", (window.innerWidth - 300) + "px", "0px");
+        document.body.appendChild(div);
+        //console.log($('nav').children())
+    }
+    retrieveGoogleSearch(){
+        var results = {};
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function(e){
+            var parser = new DOMParser ();
+            var responseDoc = parser.parseFromString (oReq.response, "text/html");
+            var organicResults = responseDoc.getElementsByClassName("r");
+            for (var i=0; i < organicResults.length; i++){
+                var link = organicResults[i].firstChild;
+                results[link.lastChild.textContent]=link.getAttribute('href');
+            }
+            console.log(results);
+        }
+        oReq.open("GET","https://www.google.com/search?q=estudiantes");
+        oReq.send();   
+    }
+    retrieveBingSearch(){
+        var results = {};
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function(e){
+            var parser = new DOMParser ();
+            var responseDoc = parser.parseFromString (oReq.response, "text/html");
+            var organicResults = responseDoc.getElementsByClassName("b_algo");
+            for (var i=0; i < organicResults.length; i++){
+                var link = organicResults[i].firstChild.firstChild;
+                results[link.textContent]=link.getAttribute('href');
+            }
+            console.log(results);
+        }
+        oReq.open("GET","https://www.bing.com/search?q=estudiantes");
+        oReq.send();   
+    }
+  
+  /*  retrieveDuckDuckGoSearch(){
+        console.log('hola')
+        var results = {};
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function(e){
+            var parser = new DOMParser ();
+            var responseDoc = parser.parseFromString (oReq.response, "text/html");
+            console.log(responseDoc)
+            var organicResults = responseDoc.getElementsByClassName("result");
+            console.log(organicResults)
+            for (var i=0; i < organicResults.length; i++){
+                var link = organicResults[i];
+                console.log(link)
+                //results[link.textContent]=link.getAttribute('href');
+            }
+            //console.log(results);
+        }
+        oReq.open("GET","https://duckduckgo.com/?q=estudiantes&t=h_&ia=web");
+        oReq.send();   
+    }
+    */
+    createContainer(width, height, left, top) {
+        var div = document.createElement("div");
+        div.style.width = width;
+        div.style.height = height;
+        div.style.position = "fixed";
+        div.style.background = "white";
+        div.style.top = top;
+        div.style.left = left;
+        div.style.zIndex = this.getMaxZindex() + 1;
+
+        div.onclick = function () {
+            this.remove();
+        }
+        return div;
+    }
+    getMaxZindex() {
+        return Array.from(document.querySelectorAll('body *'))
+            .map(a => parseFloat(window.getComputedStyle(a).zIndex))
+            .filter(a => !isNaN(a)).sort().pop();
     }
 }
 
@@ -11,8 +88,8 @@ var pageManager = new Result();
 
 //Listening for background's messages
 browser.runtime.onMessage.addListener((request, sender) => {
-	console.log("[content-side] calling the message: " + request.call);
-	if(pageManager[request.call]){
-		pageManager[request.call](request.args);
-	}
+    console.log("[content-side] calling the message: " + request.call);
+    if (pageManager[request.call]) {
+        pageManager[request.call](request.args);
+    }
 });
