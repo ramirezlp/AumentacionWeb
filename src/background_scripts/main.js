1fda6abf14f9c544d82401b88a7a5c846cfe08fc
ramirezlp <agusedlp1@gmail.com>
2020-06-04 14:32:29 -0300

    saco json.stringify

--- src/background_scripts/main.js
+++ src/background_scripts/main.js
@@ -183,7 +183,7 @@ class BackgroundResult extends AbstractP2PExtensionBackground {
     docs.dato = args[0];
     docs.automatic = true;
     docs.withoutcheck = true;
-    this.sendRequest(JSON.stringify(docs), "All");
+    this.sendRequest(docs, "All");
   }
 
   parsearString(busqueda) {
@@ -231,14 +231,14 @@ class BackgroundResult extends AbstractP2PExtensionBackground {
       await engine.asyncRetrieveSearch(msg.dato).then((jsonNews) => {
         console.log("News obtained, preparing to send response");
         console.log(jsonNews);
-        this.sendResponse(JSON.stringify({
+        this.sendResponse({
             metodo: "engineResults",
             results: jsonNews,
             buscador: msg.buscadorUtilizado,
             className: engine.organicResultClass,
             automatic: true,
             withoutcheck: true,
-          }),
+          },
           peer
         );
         console.log("Response sent");
